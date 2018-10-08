fcfef16af961df16c048726207f8de43035b4e67
enkh0001 <maaraagiingirl@gmail.com>
2018-10-08 17:24:31 +0800

    Update main.R

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ options(warn=-1)
 
 url <- "https://www.tripadvisor.com.sg/Attraction_Review-g294265-d324752-Reviews-Esplanade_Theatres_on_the_Bay-Singapore.html"
 
-morepglist <-seq(10,680,10)
+morepglist <-seq(10,700,10)
 
 pick <- url
 # get list of urllinks corresponding to different pages
@@ -24,7 +24,7 @@ for(i in 1:length(morepg)){
 }
 head(urllink)
 Review_Data <- data.frame()
-for (i in 1:70) {
+for (i in 1:71) {
   reviews <- urllink[i] %>%
     read_html() %>%
     html_nodes("#REVIEWS .ui_column.is-9")
@@ -39,10 +39,15 @@ for (i in 1:70) {
     html_attr("title") %>%
     strptime("%d %b %Y") %>%
     as.POSIXct()
+    numeric_rating<- reviews %>%
+    html_node(".ui_bubble_rating")%>%
+    as.character()%>%
+   substr(start=38,stop=39)%>%
+   as.numeric()/10
   review <- reviews %>%
     html_node(".entry .partial_entry") %>%
     html_text()
   webpage <- read_html(urllink[i])
-  Review_Data <- rbind(Review_Data, data.frame(review_id, title, rating_date, review, stringsAsFactors = FALSE))
+  Review_Data <- rbind(Review_Data, data.frame(review_id, title, rating_date, numeric_rating, review, stringsAsFactors = FALSE))
 }
  write.csv(Review_Data, file = "/data/out/tables/Review_Data.csv", row.names = FALSE)
